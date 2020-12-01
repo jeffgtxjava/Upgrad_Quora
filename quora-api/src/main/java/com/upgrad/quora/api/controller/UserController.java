a95c0da36c704b0130cd894d6a761601ad411a0c
jeffgtxjava <jeffgtxjava@gmail.com>
2020-12-01 20:36:52 +0530

    userEntity has been created locally to read from the request call made

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -4,25 +4,48 @@ package com.upgrad.quora.api.controller;
 import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
 import com.upgrad.quora.service.entity.UserEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping(path = "/user")
 public class UserController {
+
+
+    // endpoint for signup
 
     @RequestMapping(path = "/signup",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SignupUserResponse> signup (final SignupUserRequest signupUserRequest){
 
+        // store the user request details
+
         final UserEntity userEntity= new UserEntity();
+        userEntity.setUuid(UUID.randomUUID().toString());
+        userEntity.setFirstName(signupUserRequest.getFirstName());
+        userEntity.setFirstName(signupUserRequest.getFirstName());
+        userEntity.setLastName(signupUserRequest.getLastName());
+        userEntity.setUsername(signupUserRequest.getUserName());
+        userEntity.setEmail(signupUserRequest.getEmailAddress());
+        userEntity.setPassword(signupUserRequest.getPassword());
+        userEntity.setSalt("abc1234");
+        userEntity.setCountry(signupUserRequest.getCountry());
+        userEntity.setAboutme(signupUserRequest.getAboutMe());
+        userEntity.setDob(signupUserRequest.getDob());
+        userEntity.setRole("nonadmin");
+        userEntity.setContactNumber(signupUserRequest.getContactNumber());
 
+        // check with username and email for the existence of it in the database
+        // if these are not unique throw errors accordingly
 
-
-        return null;
+        //return the success code after the user account get's created successfully
+        return new ResponseEntity<SignupUserResponse>(userResponse,HttpStatus.CREATED);
     }
 
 }
