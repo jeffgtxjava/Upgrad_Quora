d92e58773e19c7cdc85a26be3335305afde13b72
jeffgtxjava <jeffgtxjava@gmail.com>
2020-12-01 20:52:08 +0530

    created an entity manager for connecting to db along with get user by username and email

---
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -0,0 +1,36 @@
+package com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.UserEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    // get the user from the username
+    public UserEntity getUserByUsername(final String username) {
+        try {
+            return entityManager.createNamedQuery("userByUsername",UserEntity.class).setParameter("username",username).getSingleResult();
+        }catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+
+    // get the user from the email
+    public UserEntity getUserByEmail(final String email) {
+        try {
+            return entityManager.createNamedQuery("userByEmail",UserEntity.class).setParameter("email",email).getSingleResult();
+        }catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+}
