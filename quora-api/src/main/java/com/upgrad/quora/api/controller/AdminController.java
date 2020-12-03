8042f4078929c5a78ea3d723be219a1a03b89893
jeffgtxjava <jeffgtxjava@gmail.com>
2020-12-03 13:57:41 +0530

    AdminController has been created

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AdminController.java
@@ -0,0 +1,45 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.UserDeleteResponse;
+import com.upgrad.quora.service.business.UserAdminBusinessService;
+import com.upgrad.quora.service.business.UserDeleteBusinessService;
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AdminController {
+
+    @Autowired
+    private UserAdminBusinessService userAdminBusinessService;
+
+    @Autowired
+    private UserDeleteBusinessService userDeleteBusinessService;
+
+    @RequestMapping(path = "/admin/user/{userId}",method = RequestMethod.DELETE,produces =
+            MediaType.APPLICATION_JSON_UTF8_VALUE,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<UserDeleteResponse> deleteUserByUuid(@PathVariable("userId") String uuid, @RequestHeader(
+            "accessToken") String accessToken) throws AuthorizationFailedException, UserNotFoundException {
+
+        String[] bearerToken = accessToken.split("Bearer ");
+
+        final UserEntity signedInUserEntity = userAdminBusinessService.getUser(uuid,bearerToken[1]);
+        final UserEntity userToDelete = userDeleteBusinessService.getUserByUuid(uuid);
+
+
+        String deletedUuid = userDeleteBusinessService.deleteUserByUuid(signedInUserEntity,userToDelete);
+
+        UserDeleteResponse userDeleteResponse = new UserDeleteResponse().id(deletedUuid).status("USER " +
+                "SUCCESSFULLY DELETED");
+
+
+
+        return new ResponseEntity<>(userDeleteResponse, HttpStatus.OK);
+    }
+}
